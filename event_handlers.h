69e12691c7a50d8e286cb90556c19b1e77b79b2b
Andrew Stocker <amstocker@dons.usfca.edu>
2016-02-07 13:08:37 -0800

    minor

--- event_handlers.h
+++ event_handlers.h
@@ -11,7 +11,7 @@
 typedef Status (*EventHandler) (Event *event);
 
 #define EVENT_MAP \
-  { "message", 7, message_event_handler }
+  { "message"  , 7       , message_event_handler    }
 
 
 // X_event_handler
